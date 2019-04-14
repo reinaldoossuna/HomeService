5125750339367edf468c86393deefb99a96bf08f
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2019-04-14 16:53:11 -0400

    setting test launch

---
+++ test_slam.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+x-terminal-emulator -x roslaunch turtlebot_gazebo turtlebot_world.launch &
+
+sleep 5 &&
+
+x-terminal-emulator -x rosrun gmapping slam_gmapping  &
+
+sleep 5 &&
+
+x-terminal-emulator -x roslaunch turtlebot_rviz_launchers view_navigation.launch &
+
+sleep 5 &&
+
+x-terminal-emulator -x roslaunch turtlebot_teleop keyboard_teleop.launch 
