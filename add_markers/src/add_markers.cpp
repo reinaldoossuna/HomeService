67ebc44363c74508f51c5eb7fa4cd06d14adaec1
Reinaldo Ossuna <reinaldo.ossuna@gmail.com>
2019-04-25 09:30:44 -0400

    homeservice

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -1,5 +1,50 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include "nav_msgs/Odometry.h"
+#include <math.h>
+
+float PICK_UP_X = 7.70;
+float PICK_UP_Y = 0.37;
+
+float DROP_OFF_X = 0.00;
+float DROP_OFF_Y = 1.30;
+
+// state 0 = going to pickup
+// state 1 = going to dropoff
+// state 2 = droped
+int state = 0;
+
+void odomCallback(const nav_msgs::Odometry::ConstPtr &msg)
+{
+
+  float pos_x = msg->pose.pose.position.x;
+  float pos_y = msg->pose.pose.position.y;
+
+  float dist = 0.0;
+
+  switch(state){
+    case 0:
+             dist = sqrt(pow((PICK_UP_X - pos_x),2) + pow((PICK_UP_Y - pos_y),2));
+             ROS_INFO("%f",dist);
+             if(dist <= 0.5){
+               ROS_INFO("In the pick up");
+               state = 1;
+             }
+             break;
+   
+    case 1:
+             dist = sqrt(pow((DROP_OFF_X - pos_x),2) + pow((DROP_OFF_Y - pos_y),2));
+             ROS_INFO("%f",dist);
+             if(dist <= 0.5){
+               ROS_INFO("In the dropoff");
+               state = 2;
+             }
+             break;
+
+  }
+
+}
+
 
 int main( int argc, char** argv )
 {
@@ -7,6 +52,8 @@ int main( int argc, char** argv )
   ros::NodeHandle n;
   ros::Rate r(1);
   ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
+
+  ros::Subscriber odom_sub = n.subscribe("/odom",1000, odomCallback);
 
   // Set our initial shape type to be a cube
   uint32_t cube = visualization_msgs::Marker::CUBE;
@@ -28,15 +75,15 @@ int main( int argc, char** argv )
   marker.action = visualization_msgs::Marker::ADD;
 
   // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-  marker.pose.position.x = 7.70;
-  marker.pose.position.y = 0.37;
+  marker.pose.position.x = PICK_UP_X;
+  marker.pose.position.y = PICK_UP_Y;
   marker.pose.position.z = 0.0;
   marker.pose.orientation.x = 0.0;
   marker.pose.orientation.y = 0.0;
   marker.pose.orientation.z = 0.0;
   marker.pose.orientation.w = 1.0;
 
-  // Set the scale of the marker -- 1x1x1 here means 1m on a side
+  // Set the scale of the marker 
   marker.scale.x = 0.10;
   marker.scale.y = 0.10;
   marker.scale.z = 0.10;
@@ -62,24 +109,23 @@ int main( int argc, char** argv )
       ROS_WARN_ONCE("Please create a subscriber to the marker");
       sleep(1);
     }
-
+    switch(state){
+      case 0:
+              ROS_INFO("going to pick up");
+              break;
+      case 1: marker.action = visualization_msgs::Marker::DELETE;
+              ros::Duration(5.0).sleep();
+              ROS_INFO("going to dropoff");
+              break;
+      case 2: 
+              marker.pose.position.x = DROP_OFF_X;
+              marker.pose.position.y = DROP_OFF_Y;
+              marker.action = visualization_msgs::Marker::ADD;
+              ROS_INFO("droped");
+              ros::Duration(5.0).sleep();
+              break;
+    }
     marker_pub.publish(marker);
-
-    ros::Duration(5.0).sleep();
-
-    marker.action = visualization_msgs::Marker::DELETE;
-
-    marker_pub.publish(marker);
-
-    ros::Duration(5.0).sleep();
-
-    marker.pose.position.x = 0.00;
-    marker.pose.position.y = 1.30;
-    marker.pose.position.z = 0.0;
-
-    marker.action = visualization_msgs::Marker::ADD;
-
-    marker_pub.publish(marker);
-    r.sleep();
+    ros::spinOnce();
   }
 }
--- scripts/homeservice.sh
+++ scripts/homeservice.sh
@@ -8,8 +8,13 @@ x-terminal-emulator -x roslaunch turtlebot_gazebo amcl_demo.launch &
 
 sleep 5 &&
 
-x-terminal-emulator -x roslaunch turtlebot_rviz_launchers view_navigation.launch &
+x-terminal-emulator -x roslaunch turtlebot_rviz_launchers homeservice.launch &
+
+sleep 5 &&
+
+x-terminal-emulator -x rosrun pick_objects pick_objects &
 
 sleep 5 &&
 
 x-terminal-emulator -x rosrun add_markers add_markers
+
